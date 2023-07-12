57046b4820f98c9514426aadc353e78618dce1fc
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2023-07-12 17:09:18 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 import express from "express";
-import Connection from "./database/db.js";
+const mongoose = require("mongoose");
 
+// import Connection from "./database/db.js";
+
+// initialise express
 const app = express();
 const PORT = 8000;
 
-Connection();
+// Connection();
+
+//  mondodb connect
+mongoose
+.connect(
+    'mongodb://localhost:27017/usersdb',
+    process.env.MONGODB_URI, 
+    {useNewUrlParser: true, 
+    useUnifiedTopology: true}
+)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// Server listen
 app.listen(PORT, () => console.log(`Your server is running succssfully on PORT ${PORT} `));
